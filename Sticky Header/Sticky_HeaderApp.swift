c282f92e335153a1344c39fd9409ee6fa81130b3
med <theoutsider009@gmail.com>
2021-11-29 12:50:42 +0100

    Initial Commit

---
+++ Sticky Header/Sticky_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sticky_HeaderApp.swift
+//  Sticky Header
+//
+//  Created by Adam Jemni on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Sticky_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
